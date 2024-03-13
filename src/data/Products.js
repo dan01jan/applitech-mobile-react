55d81cedc91370278d21b579a61bb7d15b50a364
alexandraaquio04 <aquinoalexandra04@gmail.com>
2024-03-13 22:45:57 +0800

    Homepage

---
+++ src/data/Products.js
@@ -0,0 +1,135 @@
+const Products =[
+    {
+        _id:"1",
+        name:"sample1",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:1,
+        numReviews:4
+      
+    },
+    {
+        _id:"2",
+        name:"sample2",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample1",
+        price:100,
+        countInStock:10,
+        rating:2,
+        numReviews:4
+        
+    },
+    {
+        _id:"3",
+        name:"sample3",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:3,
+        numReviews:4
+        
+    },
+    {
+        _id:"4",
+        name:"sample4",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:4,
+        numReviews:4
+        
+    },
+    {
+        _id:"5",
+        name:"sample5",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:5,
+        numReviews:4
+        
+    },
+    {
+        _id:"6",
+        name:"sample6",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:3,
+        numReviews:4
+        
+    },
+    {
+        _id:"1",
+        name:"sample1",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:1,
+        numReviews:4
+      
+    },
+    {
+        _id:"2",
+        name:"sample2",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample1",
+        price:100,
+        countInStock:10,
+        rating:2,
+        numReviews:4
+        
+    },
+    {
+        _id:"3",
+        name:"sample3",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:3,
+        numReviews:4
+        
+    },
+    {
+        _id:"4",
+        name:"sample4",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:4,
+        numReviews:4
+        
+    },
+    {
+        _id:"5",
+        name:"sample5",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:5,
+        numReviews:4
+        
+    },
+    {
+        _id:"6",
+        name:"sample6",
+        image: "https://i.pinimg.com/236x/75/ed/ca/75edca807e95b600d3aab8f704a5e29f.jpg",
+        description:"sample",
+        price:100,
+        countInStock:10,
+        rating:3,
+        numReviews:4
+        
+    },
+]
+export default Products;
