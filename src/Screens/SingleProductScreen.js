05a0d3e3a7235c75b84ad48d2404b1a10a1dfea0
dan01jan <danize1001@gmail.com>
2024-03-14 07:04:29 +0800

    SingleProductScreen

--- src/Screens/SingleProductScreen.js
+++ src/Screens/SingleProductScreen.js
@@ -10,14 +10,23 @@ import {
   ScrollView,
   Heading,
   HStack,
-  NumberInput,
+  Spacer
 } from "native-base";
 import colors from "../color";
 import Rating from "../Components/Rating";
-import NumericInput from "react-native-numeric-input";
-import { Ionicons } from "@expo/vector-icons";
+
 function SingleProductScreen() {
-  const[value, setValue] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  const increment = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
 
   return (
     <Box safeArea flex={1} bg={colors.white}>
@@ -33,18 +42,32 @@ function SingleProductScreen() {
           Blue Blouse
         </Heading>
         <Rating value={4} />
-        {/* <HStack value={2} alignItems="center" my={5}>
-          <NumericInput
-            value={value}
-            totalWidth={140}
-            totalHeight={30}
-            iconSize={25}
-            step={1}
-            maxValue={15}
-            minValue={0}
-            borderColor={colors.lightpink}    
-          />
-        </HStack> */}
+        <HStack alignItems="center" my={5}>
+          <Button size="sm" colorScheme="pink" onPress={decrement}>-</Button>
+          <View
+            borderColor="gray.200"
+            px={2}
+            py={1}
+            width={20} // Adjust the width as needed
+          >
+            <Input
+              value={quantity.toString()}
+              fontSize="sm"
+              textAlign="center"
+            />
+          </View>
+          <Button size="sm" colorScheme="pink" onPress={increment}>+</Button>
+          <Spacer />
+          <Heading bold color={colors.black} fontSize={19}>
+            500
+          </Heading>
+        </HStack>
+        <Text lineHeight={24} fontSize={12}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </Text>
+        <Button background={colors.main} color={colors.white} mt={10}>
+          ADD TO CART
+        </Button>
       </ScrollView>
     </Box>
   );
