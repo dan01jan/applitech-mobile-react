5728a661247732aeff524dd9e0c2363f890ee9bd
eeinstein11223 <johnmatthew.dullas@tup.edu.ph>
2024-03-14 19:49:21 +0800

    place order

--- src/Components/PlaceOrderModel.js
+++ src/Components/PlaceOrderModel.js
@@ -1,26 +1,26 @@
 import React, {useState} from'react';
-import { Center, Modal, VStack, HStack, Text } from 'native-base';
+import { Center, Modal, VStack, HStack, Text, Button } from 'native-base';
 import Buttone from "./Buttone";
 import colors from "../color"
 
-const OrderInfos = [
+const OrdersInfos = [
   {
     title: "Products",
    price:123.22,
    color:"black"
   },
   {
-    title: "Products",
+    title: "Shipping",
    price:123.22,
    color:"black"
   },
   {
-    title: "Products",
+    title: "Tax",
    price:123.22,
    color:"black"
   },
   {
-    title: "Products",
+    title: "Totals",
    price:1232.22,
    color:"black"
   }
@@ -44,15 +44,36 @@ export default function PlaceOrderModel() {
             <Modal.Header>Order</Modal.Header>
             <Modal.Body>
               <VStack space={7}>
-                <HStack alignItems="center" justifyContent="space-between">
-                  <Text fontWeight="medium">Products</Text>
-                  <Text color={colors.main} bold>
-                    $234.97
-                  </Text>
-
-                </HStack>
+                {OrdersInfos.map((i, index) => (
+                  <HStack
+                  key={index}
+                  alignItems="center"
+                  justifyContent="space-between"
+                  >
+                    <Text fontWeight="medium">{i.title}</Text>
+                    <Text
+                      color={i.color === "main" ? colors.main : colors.black}
+                      bold
+                      > 
+                        ${i.price}
+                      </Text>
+                  </HStack>
+                ))}
               </VStack>
             </Modal.Body>
+            <Modal.Footer>
+              <Button flex={1} bg={colors.main} h={45} _text={{
+                color:colors.black,
+              }}
+              onPress={() => setShowModel(false)}
+              _pressed={{
+                bg: colors.main,
+              }}
+              >
+                PLACE ORDER
+              </Button>
+
+            </Modal.Footer>
           </Modal.Content>
         </Modal>
     </Center>
