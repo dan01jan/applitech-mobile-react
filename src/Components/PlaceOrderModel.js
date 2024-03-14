a084cb97ca75ff064c1525290e8b31271ae31ac4
eeinstein11223 <johnmatthew.dullas@tup.edu.ph>
2024-03-14 19:33:52 +0800

    total

--- src/Components/PlaceOrderModel.js
+++ src/Components/PlaceOrderModel.js
@@ -1,10 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState} from'react';
+import { Center, Modal, VStack, HStack, Text } from 'native-base';
+import Buttone from "./Buttone";
+import colors from "../color"
+
+const OrderInfos = [
+  {
+    title: "Products",
+   price:123.22,
+   color:"black"
+  },
+  {
+    title: "Products",
+   price:123.22,
+   color:"black"
+  },
+  {
+    title: "Products",
+   price:123.22,
+   color:"black"
+  },
+  {
+    title: "Products",
+   price:1232.22,
+   color:"black"
+  }
+]
 
 export default function PlaceOrderModel() {
+  const [showModel,setShowModel] = useState(false)
   return (
-    <View>
-      <Text>PlaceOrderModel</Text>
-    </View>
+    <Center>
+       <Buttone
+        onPress={() => setShowModel(true)}
+        bg={colors.main}
+        color={colors.black}
+        mt={5}
+        > 
+        SHOW TOTAL
+        </Buttone>
+        <Modal isOpen={showModel} onClose={() => setShowModel(false)} size="lg">
+          <Modal.Content maxWidth={350}>
+            <Modal.CloseButton/>
+            <Modal.Header>Order</Modal.Header>
+            <Modal.Body>
+              <VStack space={7}>
+                <HStack alignItems="center" justifyContent="space-between">
+                  <Text fontWeight="medium">Products</Text>
+                  <Text color={colors.main} bold>
+                    $234.97
+                  </Text>
+
+                </HStack>
+              </VStack>
+            </Modal.Body>
+          </Modal.Content>
+        </Modal>
+    </Center>
   )
 }
