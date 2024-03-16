3bc6d28da2ea1564b5dd7d297d60417fd9fcd2b4
miryan1ez <veeveepeexd@gmail.com>
2024-03-16 13:40:01 +0800

    cart screen profile screen

--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -1,18 +1,20 @@
-import { Center, Heading, Image, Text } from 'native-base'
+import { Center, Heading, Text } from 'native-base'
 import React from 'react'
 import Colors from '../color'
 import Tabs from '../Components/Profile/Tabs'
+import { View, Image } from 'react-native';
 
 function ProfileScreen() {
   return (
     <>
     <Center bg={Colors.main} pt={10} pb={6}>
-      <Image source={{uri: "https://res.cloudinary.com/do2utxjmc/image/upload/v1700884184/avatars/e7lybxmp44roqyoroqwl.jpg"}}
-      alt="profile"
-      w={24}
-      h={24}
-      resizeMode="cover"
+    <View style={{ borderRadius: 50, overflow: 'hidden' }}>
+      <Image
+        source={{ uri: "https://res.cloudinary.com/do2utxjmc/image/upload/v1700884184/avatars/e7lybxmp44roqyoroqwl.jpg" }}
+        style={{ width: 100, height: 100}}
+        resizeMode="cover"
       />
+    </View>
       <Heading bold fontSize={15} isTruncated my={2} color={Colors.white}>
         User Me
       </Heading>
