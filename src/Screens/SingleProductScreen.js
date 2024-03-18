3583d61b1d78acdd75bfd206d7fa2570d1f20813
alexandraaquio04 <aquinoalexandra04@gmail.com>
2024-03-18 23:02:24 +0800

    Single Product Details

--- src/Screens/SingleProductScreen.js
+++ src/Screens/SingleProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Image,
@@ -16,12 +16,26 @@ import colors from "../color";
 import Rating from "../Components/Rating";
 import Buttone from "../Components/Buttone";
 import Review from "../Components/Review";
+import Carousel, { Pagination } from 'react-native-snap-carousel'; 
 import { useNavigation } from '@react-navigation/native'
 
-function SingleProductScreen({route}) {
+function SingleProductScreen({ route }) {
   const [quantity, setQuantity] = useState(1);
-  const navigation = useNavigation()
-  const product = route.params
+  const navigation = useNavigation();
+  const product = route.params;
+
+  const [availability, setAvailability] = useState('');
+  const [availabilityText, setAvailabilityText] = useState("");
+
+  useEffect(() => {
+    if (product.countInStock === 0) {
+      setAvailabilityText("Unavailable");
+    } else if (product.countInStock <= 5) {
+      setAvailabilityText("Limited Stock");
+    } else {
+      setAvailabilityText("Available");
+    }
+  }, [product.countInStock]);
 
   const increment = () => {
     setQuantity(quantity + 1);
@@ -33,27 +47,54 @@ function SingleProductScreen({route}) {
     }
   };
 
+  const renderCarouselItem = ({ item }) => {
+    return (
+      <Image
+        source={{ uri: item }}
+        alt={product.name}
+        w="100%"
+        h={200}
+        resizeMode="cover"
+      />
+    );
+  };
+
   return (
     <Box safeArea flex={1} bg={colors.white}>
       <ScrollView px={5} showsVerticalScrollIndicator={false}>
-        <Image
-          source={{ uri: product.image }} 
-          alt="Image"
-          w="full"
-          h={300}
-          resizeMode="contain"
+        <Carousel
+          data={product.images}
+          renderItem={renderCarouselItem}
+          sliderWidth={300}
+          itemWidth={300}
+          layout="default"
+          loop={true}
+        />
+        <Pagination
+          dotsLength={product.images.length}
+          activeDotIndex={0} // Set initial active dot index
+          containerStyle={{ marginTop: -20 }}
+          dotStyle={{
+            width: 10,
+            height: 10,
+            borderRadius: 5,
+            backgroundColor: 'rgba(255, 255, 255, 0.92)'
+          }}
+          inactiveDotOpacity={0.4}
+          inactiveDotScale={0.6}
         />
         <Heading bold fontSize={15} mb={2} lineHeight={22}>
           {product.name}
         </Heading>
-        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+        <Rating value={product.ratings} text={`${product.numReviews} reviews`} />
         <HStack alignItems="center" my={5}>
           <Button size="sm" colorScheme="pink" onPress={decrement}>-</Button>
           <View
             borderColor="gray.200"
+
             px={2}
             py={1}
-            width={20} // Adjust the width as needed
+            width={20}
           >
             <Input
               value={quantity.toString()}
@@ -63,20 +104,25 @@ function SingleProductScreen({route}) {
           </View>
           <Button size="sm" colorScheme="pink" onPress={increment}>+</Button>
           <Spacer />
-         
           <Spacer />
           <Heading bold color={colors.black} fontSize={19}>
-            500
+            {product.price}
           </Heading>
         </HStack>
+        
+        <Text>
+          <Text bold>Availability: </Text>{availabilityText} {product.countInStock > 0 && `(Stock: ${product.countInStock})`}</Text>
         <Text lineHeight={24} fontSize={12}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <Text bold>Description: </Text>{product.description}
         </Text>
-        <Buttone bg={colors.main} color={colors.white} mt={10}>
-          ADD TO CART
-        </Buttone>
-        <Review/>
-       
+        {product.countInStock > 0 ? (
+          <Buttone bg={colors.main} color={colors.white} mt={10}>
+            ADD TO CART
+          </Buttone>
+        ) : (
+          <Text style={{ marginTop: 20 }}>Currently Unavailable</Text>
+        )}
+        <Review />
       </ScrollView>
     </Box>
   );
