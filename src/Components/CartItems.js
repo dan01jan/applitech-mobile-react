33a5fb5016c4557003056e69cab32a286f597278
miryan1ez <veeveepeexd@gmail.com>
2024-03-19 18:15:12 +0800

    CART ITEMS

--- src/Components/CartItems.js
+++ src/Components/CartItems.js
@@ -1,48 +1,38 @@
 import React from 'react';
-import { Box, Button, Center, HStack, Image, VStack, Pressable ,Text} from 'native-base';
+import { Box, Pressable, Text, VStack, HStack, Center, Image } from 'native-base'; // Import Text component
 import { SwipeListView } from 'react-native-swipe-list-view';
 import { useSelector, useDispatch } from 'react-redux';  
 import Colors from '../color';
 import { FontAwesome } from '@expo/vector-icons';
 import { removeFromCart, clearCart } from '../../Redux/Actions/cartActions';
+import Buttone from './Buttone';
 
 const CartItems = () => {
   const cartItems = useSelector(state => state.cartItems);
   const dispatch = useDispatch();  
 
-  const handleIncrementQuantity = () => {
-    // Dispatch an action to increment the quantity of the item in the cart
-    // This action should update the quantity of the item in the Redux store
-    // For example, you might dispatch an action like incrementItemQuantity(data.item)
-};
+  const handleDeleteItem = (item) => {
+    console.log('Removing item:', item);
+    dispatch(removeFromCart(item));  
+  };
 
-const handleDecrementQuantity = () => {
-    // Dispatch an action to decrement the quantity of the item in the cart
-    // This action should update the quantity of the item in the Redux store
-    // For example, you might dispatch an action like decrementItemQuantity(data.item)
-};
-  const renderItems = (data) => {
-    const handleDeleteItem = () => {
-      console.log('Removing item:', data.item);
-      dispatch(removeFromCart(data.item));  // Dispatch the removeFromCart action with the item as argument
-    };
-
+  const renderItems = ({ item }) => {
     return (
-      <Pressable onPress={handleDeleteItem}>
+      <Pressable onPress={() => handleDeleteItem(item)}>
         <Box ml={6} mb={3}>
           <HStack alignItems="center" bg={Colors.white} shadow={1} rounded={10} overflow="hidden">
             <Center w="25%" bg={Colors.deepGray}>
-              <Image source={{ uri: data.item.images.url }} alt={data.item.name} w="full" h={24} resizeMode="contain" />
+              <Image source={{ uri: item.images[0] }} alt={item.name} w="full" h={24} resizeMode="contain" />
             </Center>
             <VStack w='60%' px={2} space={2}>
               <Text isTruncated color={Colors.black} bold fontSize={10}>
-                {data.item.name}
+                {item.name}
               </Text>
               <Text bold color={Colors.lightblack}>
-                Price: {data.item.price}
+                Price: {item.price}
               </Text>
               <Text bold color={Colors.lightblack}>
-                Quantity: {data.item.quantity}
+                Quantity: {item.quantity}
               </Text>
             </VStack>
           </HStack>
@@ -55,44 +45,43 @@ const handleDecrementQuantity = () => {
     dispatch(clearCart()); 
   };
   
-  const hiddenItem = (data) => {
+  const hiddenItem = ({ item }) => {
     const handleDeleteItem = () => {
-      console.log('Removing item:', data.item);
-      dispatch(removeFromCart(data.item));  // Dispatch the removeFromCart action with the item as argument
+      console.log('Removing item:', item);
+      dispatch(removeFromCart(item));  
     };
   
     return (
       <Pressable onPress={handleDeleteItem}>
-          <Box ml={6} mb={3}>
-              <HStack alignItems="center" bg={Colors.white} shadow={1} rounded={10} overflow="hidden">
-                  <Center w="25%" bg={Colors.deepGray}>
-                      <Image source={{ uri: data.item.image}} alt={data.item.name} w="full" h={24} resizeMode="contain" />
-                  </Center>
-                  <VStack w='60%' px={2} space={2}>
-                      <Text isTruncated color={Colors.black} bold fontSize={10}>
-                          {data.item.name}
-                      </Text>
-                      <Text bold color={Colors.lightblack}>
-                          Price: {data.item.price}
-                      </Text>
-                      <Text bold color={Colors.lightblack}>
-                          Quantity: {data.item.quantity}
-                      </Text>
-                  </VStack>
-                  <Pressable onPress={handleDeleteItem} w={50} roundedTopRight={10} roundedBottomRight={10} h='88%' ml='auto' justifyContent="center" bg={Colors.red}>
-        <Center alignItems='center' space={2}>
-          <FontAwesome name="trash" size={30} color={Colors.white} />
-        </Center>
+        <Box ml={6} mb={3}>
+          <HStack alignItems="center" bg={Colors.white} shadow={1} rounded={10} overflow="hidden">
+            <Center w="25%" bg={Colors.deepGray}>
+              <Image source={{ uri: item.images[0] }} alt={item.name} w="full" h={24} resizeMode="contain" />
+            </Center>
+            <VStack w='60%' px={2} space={2}>
+              <Text isTruncated color={Colors.black} bold fontSize={10}>
+                {item.name}
+              </Text>
+              <Text bold color={Colors.lightblack}>
+                Price: {item.price}
+              </Text>
+              <Text bold color={Colors.lightblack}>
+                Quantity: {item.quantity}
+              </Text>
+            </VStack>
+            <Pressable onPress={handleDeleteItem} w={50} roundedTopRight={10} roundedBottomRight={10} h='88%' ml='auto' justifyContent="center" bg={Colors.red}>
+              <Center alignItems='center' space={2}>
+                <FontAwesome name="trash" size={30} color={Colors.white} />
+              </Center>
+            </Pressable>
+          </HStack>
+        </Box>
       </Pressable>
-
-              </HStack>
-          </Box>
-      </Pressable>
-  );
-};
+    );
+  };
 
   return (
-    <Box mr={6}>
+    <Box>
       <SwipeListView
         rightOpenValue={-50}
         previewRowKey="0"
@@ -103,7 +92,14 @@ const handleDecrementQuantity = () => {
         renderHiddenItem={hiddenItem}
         showsVerticalScrollIndicator={false}
       />
-      <Button onPress={handleClearCart}>Clear Cart</Button>
+      <Center px={5} position="justify" bottom={0} left={0} right={0} zIndex={1}>
+        <Buttone bg={Colors.main} color={Colors.white} mt={2} onPress={handleClearCart}>
+          <HStack space={2} alignItems="center">
+            <FontAwesome name="trash" size={20} color={Colors.white} />
+            <Text color={Colors.white}>REMOVE ALL</Text>
+          </HStack>
+        </Buttone>
+      </Center>
     </Box>
   );
 }
