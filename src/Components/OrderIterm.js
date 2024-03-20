b03f7cda42527eac8a31de9dc0e1c6b9f700b0bb
alexandraaquio04 <aquinoalexandra04@gmail.com>
2024-03-20 21:32:12 +0800

    placeorder

--- src/Components/OrderIterm.js
+++ src/Components/OrderIterm.js
@@ -1,56 +1,106 @@
-import { Box, Center, FlatList, Image, Pressable, VStack, Text, Button } from 'native-base'
-import React from 'react'
-import { HStack } from 'native-base'
-import Products from "../data/Products";
-import Colors from "../color"
+import React from 'react';
+import { Box, Pressable, Text, VStack, HStack, Center, Image } from 'native-base';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import { useSelector, useDispatch } from 'react-redux';  
+import Colors from '../color';
+import { FontAwesome } from '@expo/vector-icons';
+import { removeFromCart, clearCart } from '../../Redux/Actions/cartActions';
+import Buttone from './Buttone';
+import CartEmpty from './CartEmpty'; // Import the CartEmpty component
 
-export default function OrderIterm() {
-  return (
-    <FlatList
-      showsVerticalScrollIndicator={false}
-      data={Products.slice(0,3)}
-      keyExtractor={(item) => item._id}
-      renderItem={({item}) => 
-      <Pressable>
-        <Box mb={3}>
-          <HStack
-            alignItems={"center"}
-            bg={Colors.white}
-            shadow={1}
-            rounded={10}
-            overflow="hidden"
-            >
-              <Center w="25%" bg={Colors.deepGray}>
-                <Image
-                  source={{uri: item.image}}
-                  alt={item.name}
-                  w="full"
-                  h={24}
-                  resizeMode="contain"
-                  />  
-              </Center>
-              <VStack w="60%" px={2}>
-                <Text isTruncated color={Colors.black} bold fontSize={12}>
-                  {item.name}
-                </Text>
-                <Text color={Colors.lightblack} mt={2} bold>
-                  ${item.price}
-                </Text>
-              </VStack>
-              <Center>
-                <Button
-                  bg={Colors.main}
-                  _pressed={{ bg: Colors.main }}
-                  _text={{
-                    color: Colors.white,
-                  }}
-                  >
-                    5
-                  </Button>
-              </Center>
-            </HStack>
+const OrderItems = () => {
+  const cartItems = useSelector(state => state.cartItems);
+  const dispatch = useDispatch();  
+
+  const handleDeleteItem = (item) => {
+    console.log('Removing item:', item);
+    dispatch(removeFromCart(item));  
+  };
+
+  const renderItems = ({ item }) => {
+    return (
+      <Pressable onPress={() => handleDeleteItem(item)}>
+        <Box ml={6} mb={3}>
+          <HStack alignItems="center" bg={Colors.white} shadow={1} rounded={10} overflow="hidden">
+            <Center w="25%" bg={Colors.deepGray}>
+              <Image source={{ uri: item.images[0] }} alt={item.name} w="full" h={24} resizeMode="contain" />
+            </Center>
+            <VStack w='60%' px={2} space={2}>
+              <Text isTruncated color={Colors.black} bold fontSize={10}>
+                {item.name}
+              </Text>
+              <Text bold color={Colors.lightblack}>
+                Price: ₱{item.price}
+              </Text>
+              <Text bold color={Colors.lightblack}>
+                Quantity: {item.quantity}
+              </Text>
+            </VStack>
+          </HStack>
         </Box>
-      </Pressable>}
+      </Pressable>
+    );
+  };
+
+  const handleClearCart = () => {
+    dispatch(clearCart()); 
+  };
+  
+  const hiddenItem = ({ item }) => {
+    const handleDeleteItem = () => {
+      console.log('Removing item:', item);
+      dispatch(removeFromCart(item));  
+    };
+  
+    return (
+      <Pressable onPress={handleDeleteItem}>
+        <Box ml={6} mb={3}>
+          <HStack alignItems="center" bg={Colors.white} shadow={1} rounded={10} overflow="hidden">
+            <Center w="25%" bg={Colors.deepGray}>
+              <Image source={{ uri: item.images[0] }} alt={item.name} w="full" h={24} resizeMode="contain" />
+            </Center>
+            <VStack w='60%' px={2} space={2}>
+              <Text isTruncated color={Colors.black} bold fontSize={10}>
+                {item.name}
+              </Text>
+              <Text bold color={Colors.lightblack}>
+                Price: ₱{item.price}
+              </Text>
+              <Text bold color={Colors.lightblack}>
+                Quantity: {item.quantity}
+              </Text>
+            </VStack>
+            <Pressable onPress={handleDeleteItem} w={50} roundedTopRight={10} roundedBottomRight={10} h='88%' ml='auto' justifyContent="center" bg={Colors.red}>
+              <Center alignItems='center' space={2}>
+                <FontAwesome name="trash" size={30} color={Colors.white} />
+              </Center>
+            </Pressable>
+          </HStack>
+        </Box>
+      </Pressable>
+    );
+  };
+
+  return (
+<Box>
+ 
+    <Center>
+      <SwipeListView
+        rightOpenValue={-50}
+        previewRowKey="0"
+        previewDuration={-40}
+        previewOpenDelay={3000}
+        data={cartItems}
+        renderItem={renderItems}
+        renderHiddenItem={hiddenItem}
+        showsVerticalScrollIndicator={false}
       />
-  )
+    </Center>
+  
+
+</Box>
+
+  );
 }
+
+export default OrderItems;
--- src/Screens/CartScreen.js
+++ src/Screens/CartScreen.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Box, Text, Center, ScrollView, HStack, Button } from 'native-base';
-import colors from '../color';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeFromCart, clearCart } from '../../Redux/Actions/cartActions'; // Assuming you have these action creators
-import CartItems from '../Components/CartItems';
-import Buttone from '../Components/Buttone';
+import React from "react";
+import { Box, Text, Center, ScrollView, HStack, Button } from "native-base";
+import colors from "../color";
+import { useSelector, useDispatch } from "react-redux";
+import { removeFromCart, clearCart } from "../../Redux/Actions/cartActions"; // Assuming you have these action creators
+import CartItems from "../Components/CartItems";
+import Buttone from "../Components/Buttone";
 import { useNavigation } from "@react-navigation/native";
 
 function CartScreen() {
   const navigation = useNavigation();
-  const cartItems = useSelector(state => state.cartItems);
+  const cartItems = useSelector((state) => state.cartItems);
   const dispatch = useDispatch();
 
   // Calculate total by considering quantity and price of each item
-  const total = cartItems.reduce((acc, item) => acc + (item.quantity * item.price), 0);
+  const total = cartItems.reduce(
+    (acc, item) => acc + item.quantity * item.price,
+    0
+  );
 
   const handleCheckout = () => {
     // Dispatch action for checkout
@@ -22,45 +25,69 @@ function CartScreen() {
 
   return (
     <Box flex={1} safeAreaTop bg={colors.lightpink} position="relative">
-    
       <Center w="full" py={5}>
-        <Text color={colors.black} fontSize={20} bold>CART</Text>
+        <Text color={colors.black} fontSize={20} bold>
+          CART
+        </Text>
       </Center>
 
-      <ScrollView showsVerticalScrollIndicator={false} style={{ marginBottom: 60 }}>
-        <CartItems cartItems={cartItems} /> 
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{ marginBottom: 60 }}
+      >
+        <CartItems cartItems={cartItems} />
 
         <Center mt={5} marginBottom={5}>
-          <HStack rounded={50} 
+          <HStack
+            rounded={50}
             justifyContent="space-between"
-            bg={colors.white} shadow={2} w='90%' pl={5} h={45} alignItems="center">
+            bg={colors.white}
+            shadow={2}
+            w="90%"
+            pl={5}
+            h={45}
+            alignItems="center"
+          >
             <Text>Total</Text>
             <Button
-  px={10}
-  h={45}
-  rounded={50}
-  bg={colors.main}
-  _text={{
-    color: colors.white,
-    fontWeight: "semibold",
-  }}
-  _pressed={{
-    bg: colors.main,
-  }}
->
-  <Center flex={1}>
-    <Text color={colors.white} textAlign="center"> {/* Center-align the text */}
-      {total.toFixed(2)}
-    </Text>
-  </Center>
-</Button>
-
+              px={10}
+              h={45}
+              rounded={50}
+              bg={colors.main}
+              _text={{
+                color: colors.white,
+                fontWeight: "semibold",
+              }}
+              _pressed={{
+                bg: colors.main,
+              }}
+            >
+              <Center flex={1}>
+                <Text color={colors.white} textAlign="center">
+                  {" "}
+                  {/* Center-align the text */}
+                  {total.toFixed(2)}
+                </Text>
+              </Center>
+            </Button>
           </HStack>
         </Center>
       </ScrollView>
 
-      <Center px={5} position="justify" bottom={0} left={0} right={0} zIndex={1}>
-        <Buttone bg={colors.main} color={colors.white} mt={10} onPress={() => navigation.navigate("Shipping")}>
+      <Center
+        px={5}
+        position="justify"
+        bottom={0}
+        left={0}
+        right={0}
+        zIndex={1}
+      >
+        <Buttone
+          bg={colors.main}
+          color={colors.white}
+          mt={10}
+          onPress={() => navigation.navigate("Shipping")}
+        >
           CHECKOUT
         </Buttone>
       </Center>
