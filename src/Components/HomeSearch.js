d2f308c7f581863815f2e76ca7d5c0f01363ac85
eeinstein11223 <johnmatthew.dullas@tup.edu.ph>
2024-03-20 15:32:40 +0800

    Cart Items Length

--- src/Components/HomeSearch.js
+++ src/Components/HomeSearch.js
@@ -4,10 +4,12 @@ import colors from "../color";
 import { Pressable } from "react-native";
 import { MaterialIcons, Ionicons, FontAwesome5 } from "@expo/vector-icons";
 import { useNavigation } from '@react-navigation/native'; // Import the hook
+import { useSelector } from 'react-redux'; // Import useSelector to get cartItems
 
 function HomeSearch() {
   const navigation = useNavigation(); // Use the useNavigation hook to get the navigation object
-  
+  const cartItems = useSelector(state => state.cartItems); // Get cartItems from Redux store
+
   return (
     <HStack
       space={3}
@@ -19,7 +21,7 @@ function HomeSearch() {
       safeAreaTop
     >
       <Input
-        placeholder="Cellphone,Laptop,Computer ...."
+        placeholder="Cellphone, Laptop, Computer ...."
         w="85%"
         bg={colors.white}
         type="search"
@@ -36,19 +38,21 @@ function HomeSearch() {
           size={24}
           color={colors.white}
         ></FontAwesome5>
-        <Box
+       <Box
           px={1}
+          py={0.5}
           rounded="full"
           position="absolute"
-          top={-13}
-          left={2}
+          top={-15}
+          left={5}
           bg={colors.red}
+          height={21} // Add height
           _text={{
             color: colors.white,
             fontSize: "11px",
           }}
         >
-          
+          {cartItems.length}
         </Box>
       </Pressable>
     </HStack>
