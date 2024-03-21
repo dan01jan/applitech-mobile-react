9e3663704004b9b2745a99310a432ae370223e10
dan01jan <danize1001@gmail.com>
2024-03-21 14:43:44 +0800

    BrandForm Route

--- src/Screens/Admin/Dashboard.js
+++ src/Screens/Admin/Dashboard.js
@@ -19,7 +19,7 @@ const Dashboard = () => {
   const sidebarItems = [
     { id: 1, title: 'Home', screen: 'Main' },
     { id: 2, title: 'Product', screen: 'AdminProduct' },
-    { id: 3, title: 'Brand', screen: 'AdminBrand' },
+    { id: 3, title: 'Brand', screen: 'BrandForm' },
   ];
 
   useEffect(() => {
